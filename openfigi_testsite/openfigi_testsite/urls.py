8352c6eab8000ec25f75872b50adf5bc8deb060f
Paco <franciscosprouse@gmail.com>
2019-10-22 10:38:51 -0700

    added homepage html and css for single submit

--- openfigi_testsite/openfigi_testsite/urls.py
+++ openfigi_testsite/openfigi_testsite/urls.py
@@ -14,8 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('openfigi_connect', include('openfigi_connect.urls')),
     path('admin/', admin.site.urls),
 ]
