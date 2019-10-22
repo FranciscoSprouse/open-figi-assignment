a15645132a59999e6baa93878148321c995434c9
Paco <franciscosprouse@gmail.com>
2019-10-22 12:29:06 -0700

    got file uploading to work

--- openfigi_testsite/openfigi_connect/urls.py
+++ openfigi_testsite/openfigi_connect/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name='openfigi_connect'
 urlpatterns = [
-    path('', views.home_view, name='home')
+    path('', views.home_view, name='home'),
+    path('<str:sedol>/', views.sedol_view, name='sedol')
 ]
--- openfigi_testsite/openfigi_connect/views.py
+++ openfigi_testsite/openfigi_connect/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 
 
 def home_view(request):
+    if request.FILES:
+        return render(request, 'openfigi_connect/upload_success.html')
     return render(request, 'openfigi_connect/homepage.html')
+
+
+def sedol_view(request, sedol):
+    return render(request, 'openfigi_connect/upload_success.html')
