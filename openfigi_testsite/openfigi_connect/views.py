1afcc80ab282a20928105edcb113545e70172e14
Paco <franciscosprouse@gmail.com>
2019-10-24 11:15:44 -0700

    added api calling features

--- openfigi_testsite/openfigi_connect/views.py
+++ openfigi_testsite/openfigi_connect/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 import openpyxl
+import json
+import requests
 
 def home_view(request):
     files = request.FILES
@@ -10,10 +12,24 @@ def home_view(request):
         wb = openpyxl.load_workbook(sedol)
 
         worksheet = wb["Sheet1"]
-        ids = []
+        api_url = 'https://api.openfigi.com/v2/mapping'
+        headers = {'Content-Type': 'application/json'}
+        reading_header = True
+        composites = []
         for row in worksheet.iter_rows():
+            if reading_header:
+                reading_header = False
+                continue
             for cell in row:
-                ids.append(str(cell.value))
-        return render(request, 'openfigi_connect/upload_success.html')
+                print(str(cell.value))
+                data = [{"idType": "ID_SEDOL", "idValue": str(cell.value)}]
+                response = requests.post(api_url, headers=headers, data=json.dumps(data))
+                response_data = response.json()[0]["data"]
+                for figi in response_data:
+                    composites.append(figi['compositeFIGI'])
+                break
+            break
+
+        return render(request, 'openfigi_connect/upload_success.html', context={'composites': composites})
 
     return render(request, 'openfigi_connect/homepage.html')
