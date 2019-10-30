df9e5e161f4049eeb931a446ce1dfaeb98a14889
Paco <franciscosprouse@gmail.com>
2019-10-30 09:37:37 -0700

    updated views

--- openfigi_testsite/openfigi_connect/views.py
+++ openfigi_testsite/openfigi_connect/views.py
@@ -16,20 +16,20 @@ def home_view(request):
         headers = {'Content-Type': 'application/json'}
         reading_header = True
         composites = []
+        data = []
         for row in worksheet.iter_rows():
             if reading_header:
                 reading_header = False
                 continue
             for cell in row:
-                print(str(cell.value))
-                data = [{"idType": "ID_SEDOL", "idValue": str(cell.value)}]
-                response = requests.post(api_url, headers=headers, data=json.dumps(data))
-                response_data = response.json()[0]["data"]
-                for figi in response_data:
-                    composites.append(figi['compositeFIGI'])
-                break
-            break
-
+                data.append({"idType": "ID_SEDOL", "idValue": str(cell.value)})
+        response = requests.post(api_url, headers=headers, data=json.dumps(data[10:20]))
+        if response.status_code == 200 and "data" in response.json()[0]:
+            response_data = response.json()[0]["data"]
+            for figi in response_data:
+                composites.append(figi['compositeFIGI'])
+        elif response.status_code != 200:
+            print(response.content)
         return render(request, 'openfigi_connect/upload_success.html', context={'composites': composites})
 
     return render(request, 'openfigi_connect/homepage.html')
